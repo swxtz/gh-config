f298ba93afd80dcc381a3c637b15ff661743d94c
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-01-28 15:07:01 -0300

    feat: open explorer when download is done

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -16,11 +16,20 @@ fn main() {
             "config",
             "--global",
             "user.email",
-            "dev.gustavomendona@protonmail.com",
+            "dev.gustavomendonca@protonmail.com",
         ]
         .to_vec(),
     )
     .expect("Error");
 
     download_font().expect("Erro ao baixar fonte");
+
+    let os = std::env::consts::OS;
+    if os == "windows" {
+        run_command("explorer", ["."].to_vec()).expect("Error no OS");
+    } else if os == "linux" {
+        run_command("nautilus", ["."].to_vec()).expect("Error no OS");
+    } else if os == "macos" {
+        println!("Mac OS não é suportado ainda")
+    }
 }
