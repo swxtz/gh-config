46a1918164b1ca6ecafa3ed925073a9af86d5139
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-01-28 02:16:17 -0300

    feat: remove yaml logging strategy

--- rust/src/config/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod create_file;
-pub mod os;
-pub mod read_file;
-pub mod schema;
-pub mod writer;
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,22 +1,11 @@
-mod config;
 mod logger;
 mod utils;
 
-use crate::config::os::verify_os;
 use crate::utils::commands::run_command;
 
 fn main() {
-    verify_os();
-
-    match run_command("git", ["config", "--global", "user.name", "Gustavo Mendonça"].to_vec()) {
-        Ok(_) => println!("Deu certo"),
-        Err(e) => eprint!("{}", e),
-    }
-
-    match run_command("git", ["config", "--global", "user.email", "teste@teste.com"].to_vec()) {
-        Ok(_) => println!("Deu certo"),
-        Err(e) => eprint!("{}", e),
-    }
+    run_command("git", ["config", "--global", "user.name", "Gustavo Mendonça"].to_vec()).expect("Error");
+    run_command("git", ["config", "--global", "user.email", "teste@teste.com"].to_vec()).expect("Error");
 
     println!("Hello, world!");
 }
