5dab9a033427028369e44a023a54fddb98d33157
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-03-06 20:15:10 -0300

    chore: add eslint

---
+++ node/src/cli/cli.ts
@@ -0,0 +1,4 @@
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+
---
+++ node/src/index.ts
@@ -0,0 +1,14 @@
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+yargs(hideBin(process.argv))
+    .command(
+        "curl <url>",
+        "fetch the contents of the URL",
+        () => {},
+        (argv) => {
+            console.info(argv);
+        }
+    )
+    .demandCommand(1)
+    .parse();
