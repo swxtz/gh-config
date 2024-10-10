aeca7d42ba3f1288b7fc316212f06ae080a39e45
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-10-10 17:28:31 -0300

    feat: add grazi and rafa infos

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,8 @@ fn main() {
     |                                                   |
     | 1 - Gustavo                                       |
     | 2 - Sarah                                         |
-    |                                                   |
+    | 3 - Grazi                                         |
+    | 4 - Rafael                                        |
     |                                                   |
     =====================================================
     ");
@@ -29,7 +30,9 @@ fn main() {
     match num {
         1 => run_user_one(),
         2 => run_user_two(),
-        _ => println!("Selecione um usuario valido"),
+        3 => run_user_three(),
+        4 => run_user_four(),
+        _ => println!("Selecione um usuario valido!"),
     }
 
 
@@ -58,7 +61,8 @@ fn run_user_one() {
     download_font().expect("Erro ao baixar fonte");
 
     webbrowser::open("https://github.com/cli/cli/releases/download/v2.45.0/gh_2.45.0_windows_amd64.msi").expect("Erro ao abrir navegador");
-    webbrowser::open("https://download.jetbrains.com/idea/ideaIC-2023.3.4.exe?_ga=2.15497974.1590588974.1709763899-36386924.1709763899&_gl=1*my2gq5*_ga*MzYzODY5MjQuMTcwOTc2Mzg5OQ..*_ga_9J976DJZ68*MTcwOTc2NzY4OC4yLjAuMTcwOTc2NzY4OS4wLjAuMA..").expect("Erro ao abrir navegador");
+    webbrowser::open("https://github.com/httpie/desktop/releases/download/v2024.1.2/HTTPie-Setup-2024.1.2.exe").expect("erro ao baixar httpie no usuario 1");
+    webbrowser::open("https://github.com/coreybutler/nvm-windows/releases/download/1.1.12/nvm-setup.exe").expect("erro ao baixar gh cli no usuario 1");
 
     let os = std::env::consts::OS;
     if os == "windows" {
@@ -71,8 +75,27 @@ fn run_user_one() {
 }
 
 fn run_user_two() {
-    run_command("git", ["config", "--global", "user.email", "nicaciosarah085@gmail"].to_vec()).expect("Error ao rodar run command no usuario 2");
+    run_command("git", ["config", "--global", "user.email", "nicaciosarah085@gmail.com"].to_vec()).expect("Error ao rodar run command no usuario 2");
     run_command("git", ["config", "--global", "user.name", "Sarah Nicacio"].to_vec()).expect("Error ao rodar run command no usuario 2");
     run_command("git", ["config", "--global", "init.defaultBranch", "main"].to_vec()).expect("Error ao rodar run command no usuario 2");
+    webbrowser::open("https://github.com/coreybutler/nvm-windows/releases/download/1.1.12/nvm-setup.exe").expect("erro ao baixar gh cli no usuario 2");
 
+}
+
+fn run_user_three() {
+    run_command("git", ["config", "--global", "user.email", "vieiravarandagrazielly@gmail.com"].to_vec()).expect("Error ao rodar run command no usuario 3");
+    run_command("git", ["config", "--global", "user.name", "Grazielly Varanda"].to_vec()).expect("Error ao rodar run command no usuario 3");
+    run_command("git", ["config", "--global", "init.defaultBranch", "main"].to_vec()).expect("Error ao rodar run command no usuario 3");
+    webbrowser::open("https://github.com/coreybutler/nvm-windows/releases/download/1.1.12/nvm-setup.exe").expect("erro ao baixar gh cli no usuario 3");
+    webbrowser::open("https://github.com/httpie/desktop/releases/download/v2024.1.2/HTTPie-Setup-2024.1.2.exe").expect("erro ao baixar httpie no usuario 4");
+    webbrowser::open("https://github.com/cli/cli/releases/download/v2.58.0/gh_2.58.0_windows_amd64.msi").expect("erro ao baixar gh cli no usuario 4");
+}
+
+fn run_user_four() {
+    run_command("git", ["config", "--global", "user.email", "rrafa7124@gmail.com"].to_vec()).expect("Error ao rodar run command no usuario 4");
+    run_command("git", ["config", "--global", "user.name", "Rafael Raposo"].to_vec()).expect("Error ao rodar run command no usuario 4");
+    run_command("git", ["config", "--global", "init.defaultBranch", "main"].to_vec()).expect("Error ao rodar run command no usuario 4");
+    webbrowser::open("https://github.com/httpie/desktop/releases/download/v2024.1.2/HTTPie-Setup-2024.1.2.exe").expect("erro ao baixar httpie no usuario 4");
+    webbrowser::open("https://github.com/cli/cli/releases/download/v2.58.0/gh_2.58.0_windows_amd64.msi").expect("erro ao baixar gh cli no usuario 4");
+    webbrowser::open("https://github.com/coreybutler/nvm-windows/releases/download/1.1.12/nvm-setup.exe").expect("erro ao baixar gh cli no usuario 4");
 }
